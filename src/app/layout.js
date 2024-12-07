413dc751ba64a2063a001dddd1eef6b48c22a0cd
Michael <michael@uplord.co.uk>
2024-12-07 09:59:34 +0000

    move favicon and apple touch

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,8 +11,12 @@ const dmSans = DM_Sans({
 });
 
 export const metadata = {
-  title: "Michael Allen",
-  description: "Michael Allen",
+  title: "Michael Allen - Front End Developer",
+  description: "Experienced Front End Developer with 9 years of expertise, showcasing excellent collaboration, organization, and teamwork skills. Passionate about HTML, CSS, and JavaScript, I thrive on creating exceptional websites. My strong analytical, debugging, and problem-solving abilities have successfully served both small and large clients. Always open to exploring new technologies for innovative web solutions.",
+  icons: {
+    icon: "/favicon.svg",
+    apple: "/apple.png",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -23,8 +27,6 @@ export default function RootLayout({ children }) {
         <meta name="description" content={metadata.description} />
         <meta name="supported-color-schemes" content="dark light" />
         <meta name="color-scheme" content="dark light" />
-        <link rel="icon" href="/favicon.svg" />
-        <link rel="apple-touch-icon" href="/apple.png" />
       </Head>
       <body>
         <ThemeProvider
