538b9c39829f54865212b4a8995b53e6dd5e5048
Michael <michael@uplord.co.uk>
2025-01-15 23:37:07 +0000

    error page changes

---
+++ src/components/styleguide/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import styles from './style.module.scss';
+import { Logo, Buttons } from '@/components';
+import Particles from '@/components/utils/Particles';
+
+export const ErrorPage = () => {
+  const data = {
+    title: 'Page not found',
+    content: 'We couldn\'t find the page you are looking for.',
+    buttons: [
+      { title: 'Go back to the homepage', link: '/', class: 'primary' },
+    ],
+  };
+
+  return (
+    <div className={styles.error}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <Logo className={styles.logo} />
+          <h1>{data.title}</h1>
+          <p>{data.content}</p>
+          <Buttons data={data.buttons} className={styles.buttons} />
+        </div>
+        <Particles id="particles-header" />
+      </div>
+    </div>
+  )
+}
+
