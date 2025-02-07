a9622b4a269aef95c34305474a99c0fbed1241e8
Michael <michael@uplord.co.uk>
2025-02-07 23:11:51 +0000

    storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,59 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    '../src/**/**/*.mdx',
+    '../src/**/**/*.stories.@(js|jsx|mjs|ts|tsx)'
+  ],
   addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    '@storybook/addon-styling-webpack'
   ],
   framework: {
-    name: "@storybook/nextjs",
+    name: '@storybook/nextjs',
     options: {},
   },
-  staticDirs: ["../public"],
-};
-export default config;
+  staticDirs: ['../public'],
+  webpackFinal: async (config) => {
+    // Remove default Storybook SVG handling to avoid conflicts
+    config.module.rules = config.module.rules.filter(
+      (rule) => !rule.test?.toString().includes("svg")
+    );
+
+    // Add SVGR loader (same as Next.js config)
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgo: true,
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "preset-default",
+                  params: {
+                    overrides: {
+                      removeViewBox: false,
+                      cleanupIds: false,
+                      inlineStyles: false,
+                      minifyStyles: false,
+                      mergeStyles: false,
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+}
+
+export default config
