035ec76a80bbd2af829bb87e1845e3f7752a5b96
Michael <michael@uplord.co.uk>
2025-02-08 21:57:44 +0000

    button leading trailing icons story

--- src/components/ui/Button2/Button.stories.tsx
+++ src/components/ui/Button2/Button.stories.tsx
@@ -1,7 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import * as icons from 'lucide-react'
 import { Button as ButtonComponent, ButtonProps } from '@/components/ui/Button2'
 import { Variant } from '@/types/button'
 import { Size } from '@/types/size'
+
+type AvailableIcons = keyof typeof icons
 
 const meta: Meta<typeof ButtonComponent> = {
   title: 'UI/ButtonNew',
@@ -35,14 +38,16 @@ const meta: Meta<typeof ButtonComponent> = {
       }
     },
     leadingIcon: {
-      table: {
-        disable: true,
-      }
+      control: {
+        type: 'select',
+      },
+      options: Object.keys(icons) as AvailableIcons[],
     },
     trailingIcon: {
-      table: {
-        disable: true,
-      }
+      control: {
+        type: 'select',
+      },
+      options: Object.keys(icons) as AvailableIcons[],
     },
   },
 }
